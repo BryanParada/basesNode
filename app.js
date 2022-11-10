792887c7f72e005d425788b8e91abd779d184749
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-10 14:15:21 -0300

    V32 - Retrieving from cmd

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const { crearArchivo } = require('./helpers/multiplicar')
 
 console.clear();
 
+//console.log(process.argv);
+const [,,arg3 = 'base=5'] = process.argv;
+const [, base = 5] = arg3.split('=');
+console.log(base);
 
-const base = 3;
+
+
+//const base = 3;
 
 
 
