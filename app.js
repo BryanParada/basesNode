dcd6d2f1ef01f87815a7926d01fa1f236f268402
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-11 10:48:57 -0300

    N36 - Independent yarg directory

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
 
 const { crearArchivo } = require('./helpers/multiplicar')
-const argv = require('yargs')
-                .option('b', {
-                    alias: 'base',
-                    type: 'number',
-                    demandOption: true
-                }) 
-                .option('l', {
-                    alias: 'listar',
-                    type: 'boolean',
-                    default: false,
-                    demandOption: true
-                }) 
-                .check( (argv, options ) =>{
-                    if( isNaN(argv.b)) {
-                        throw 'La base debe ser un número'
-                    }
-                    return true;
-                })
-                .argv;
+const argv = require('./config/yargs')
+ 
 
-// option('l') 
 console.clear();
 //console.log(process.argv);
-console.log(argv);
-console.log('base: yargs', argv.b);
+//console.log(argv);
+//console.log('base: yargs', argv.b);
 
 
 
